0e4702f6c8d65c98d858812ae2a8c0bf6b64712f
jansk32 <44828185+jansk32@users.noreply.github.com>
2021-11-10 17:32:57 +1100

    creating all unique colors

--- colorchallenge/src/App.js
+++ colorchallenge/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+
+  var [styles, setStyles] = useState([])
+
+  useEffect(() => {
+    var l = []
+    for (let r = 0; r < 256; r+= 8){
+      for (let g = 0; g < 256; g += 8 ){
+        for (let b = 0; b < 256; b+= 8 ){
+          var style = {};
+          // style.height = "1px";
+          // style.width = "1px";
+          style.backgroundColor = `rgb(${r},${g},${b})`;
+          style.display = "inline-flex"
+          l.push(style);
+        }
+      }
+
+    }
+    setStyles(l)
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: "256px", width: "128px", paddingLeft:"10%"}}>
+      {styles.map((s) => <div style={s} key={s.backgroundColor}></div>)}
     </div>
   );
 }
