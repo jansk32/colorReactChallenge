95e001d609dddad37e65ec2a2a9d2576f3a7bc17
jansk32 <44828185+jansk32@users.noreply.github.com>
2021-11-11 23:14:47 +1100

    array regroup color rows

--- colorchallenge/src/App.js
+++ colorchallenge/src/App.js
@@ -6,9 +6,6 @@ function App() {
 
   useEffect(() => {
     var l = []
-    var blue = []
-    var green = []
-    var red = []
     var j = 0
     for (let r = 0; r < 256; r+= 8){
       var newRow = []
@@ -16,25 +13,18 @@ function App() {
         for (let b = 0; b < 256; b+= 8 ){
           var grid ={
           style : {
-          height: "10px",
-          width: "10px",
+          // height: "5px",
+          // width: "5px",
+          height: "20px",
+          width: "40px",
           backgroundColor: `rgb(${r},${g},${b})`,
           display: "inline-flex",
-          margin: "0"
           },
           index: j
         }
         j+= 1;
         newRow.push(grid)
-          // if (r === 8 && g % 3 === 2){
-          //   blue.push(grid)
-          // }else if(r % 7 === 5 || g === 128){
-          //   green.push(grid)
-          // }else if(r % 4 === 3 || b % 5 === 3 ){
-          //   red.push(grid)
-          // }else{
-          //   l.push(grid);
-          // }
+        
         }
       }
       l.push(newRow)
@@ -43,34 +33,30 @@ function App() {
     // l.concat(blue)
     // l.concat(green)
     // l.concat(red)
-    
+    // reGroupColorRows(l)
     setStyles(l)
   },[])
 
-  // function tablize(arr){
-  //   var grids = "<div>"
-  //   for (let i = 0;i< arr.length;i++){
-  //     let elem = JSON.stringify(arr[i].style);
-  //     // let elemInd = arr[i].index
-  //     if (arr[i].index % 10 === 0){
-  //       grids = grids + "<div style={"+ elem +"}></div><br />"
-  //     }else{
-  //       grids = grids +  `"<div style={"+ elem +"}></div>`
-  //     }
-  //     console.log(grids)
-  //   }
-  //   grids += "</div>"
-  //   return grids
-    
-  // }
+  
+
+  function reGroupColorRows(arr){
+    var tmpArr = []
+    for (let i = 0; i< arr.length; i++){
+      var newArr = []
+      for (let j = 0; j < arr[i].length; j++){
+          newArr.push(arr[i][j])
+      }
+      tmpArr.push(newArr)
+    }
+    setStyles(tmpArr)
+  }
 
   return (
-    <div style={{height: "2560px", width: "1280px", paddingLeft:"5%"}}>
+    <div style={{height: "640px", width: "1280px", paddingLeft:"5%"}}>
       {styles.map((row) => 
-        <div>
+        <div style={{marginTop: "0px"}}>
           {row.map((col) => 
-          
-            <div style={col.style} key={col.index}></div>
+            <div style={col.style} key={col.index}> </div>
           )}
         </div>
       )}
